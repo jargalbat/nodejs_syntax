799bacdd94b389dd743382dd99bfe05bc691823d
jargalbat <jargalbat@gmail.com>
2022-03-28 00:23:23 +0800

    gdsgfd

--- graphql_playground/src/myModule.js
+++ graphql_playground/src/myModule.js
@@ -3,3 +3,6 @@
 const message = "Some message from myModule.js"
 
 export { message }
+
+
+// postgres://incrsravxhzhrg:7a2948277ad83b7b186cf4d354b857741739d5006cc2d8e708b6d7331fc34868@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d16jvdjr40bckd
